4212eb9487961fa11a7759bc12d680fd85a95965
aldeeorbs <albtadeabah@aol.com>
2016-12-07 19:28:00 -0500

    modified joblist adapter. Job address showing instead of longitudes and latitudes. misc improvements.

--- app/src/main/java/edu/gwinnetttech/gtcnsabuddy/JobListAdapter.java
+++ app/src/main/java/edu/gwinnetttech/gtcnsabuddy/JobListAdapter.java
@@ -1,7 +1,11 @@
 package edu.gwinnetttech.gtcnsabuddy;
 
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +13,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import edu.gwinnetttech.gtcnsabuddy.model.Job;
 
@@ -30,6 +37,7 @@ public class JobListAdapter extends ArrayAdapter<Job> {
     public View getView(int position, View view, @NonNull ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(getContext());
 
+
         if ( view == null ) {
             view = inflater.inflate(R.layout.job_list_item, parent, false);
         }
@@ -37,15 +45,44 @@ public class JobListAdapter extends ArrayAdapter<Job> {
         TextView txtJobID = (TextView) view.findViewById(R.id.job_ID);
         TextView txtJobDesc = (TextView) view.findViewById(R.id.job_desc);
         TextView txtJobAddress = (TextView) view.findViewById(R.id.job_address);
+        ImageView imgJobImage = (ImageView) view.findViewById(R.id.customer_image);
+
 
         Job job = jobList.get(position);
 
-        // Should have used Kotlin. Gotta love that ?
+        Geocoder geocoder = new Geocoder(this.getContext(), Locale.getDefault());
+
+
         if ( job.getLatitude() != null && job.getLongitude() != null) {
-            txtJobAddress.setText("Lat: " + job.getLatitude().toString() + " Long: " + job.getLongitude().toString());
-        }
+            //get full address
+            try{
+            List<Address> addressList = geocoder.getFromLocation(job.getLatitude(), job.getLongitude(), 1);
+
+                String addressLine = addressList.get(0).getAddressLine(0);
+                String city = addressList.get(0).getLocality();
+                String state = addressList.get(0).getAdminArea();
+                String zipcode = addressList.get(0).getPostalCode();
+
+                txtJobAddress.setText(addressLine + "\n" +
+                                      city + ", " + state + " " + zipcode);
+
+            }catch (IOException ex){
+                Log.e("Location error", ex.getMessage());
+                txtJobAddress.setText("Lat: " + job.getLatitude() + "     Long: " + job.getLongitude());
+            }
+
+        }else{
+            txtJobAddress.setText("Address not available. Please contact Supervisor");
+        } //end get full address
 
         txtJobID.setText(Integer.toString(job.getJobID()));
+
+        txtJobDesc.setText(job.getJobNotes());
+
+        // They are changing the service to add the pictures to JobDetails object, so it will already be available
+        // here without another call. But you will want to go ahead anc change the imgJobImage type to a NetworkImageView.
+        // Then we will use that to download the picture from the URL in the Job Object
+        imgJobImage.setImageURI(Uri.EMPTY);
 
         return view;
     }
